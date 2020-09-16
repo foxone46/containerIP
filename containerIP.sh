f6aafb86f3ee741879786bfd6aff1e076ede76fa
foxone46 <70193586+foxone46@users.noreply.github.com>
2020-09-16 08:36:33 +0200

    Update containerIP.sh
    + port

--- containerIP.sh
+++ containerIP.sh
@@ -5,5 +5,7 @@
 echo -e "\e[5mTo find container IP for vpntransmission\e[0m"
 echo -e "\e[41m wget http://ipecho.net/plain -O - -q ; echo \e[0m"
 echo -e "\e[5mIs the Command inside the container\e[0m"
-
+echo -e "\e[32m The ip is : \e[0m"
 docker exec -it vpntransmission bash -c "wget http://ipecho.net/plain -O - -q ; echo "
+echo -e "\e[32m :9091 \e[0m"
+
